ebe049bc624353b539962acd3f97ae4477fa407b
Benjamin Trenery <56807224+BenjaminTrenery@users.noreply.github.com>
2023-10-03 02:17:26 +0000

    shell program done

---
+++ CPSC 380/shell/README.md
@@ -0,0 +1,17 @@
+Class CPSC 380-01
+By: Benjamin Trenery
+Email: trenery@chapman.edu
+ID: 2354938
+
+Errors: not an error but after doing a command there is no "osh> " on the next line without entering a empty input to reset it
+
+Sample Output:
+* osh> ls
+* osh> README.md a.out sshell.c test.c
+
+Sites Used: 
+https://www.geeksforgeeks.org/char-comparision-in-c/ , https://www.geeksforgeeks.org/strtok-strtok_r-functions-c-examples/
+
+Files Given:
+sshell.c, README
+
--- CPSC 380/shell/sshell.c
+++ CPSC 380/shell/sshell.c
@@ -12,54 +12,65 @@ int main(void) {
     char *args[MAX_LINE/2 + 1]; /* command line arguments */
     int should_run = 1; /* flag to determine when to exit program*/
     int inputSize = 100;
-
-    pid_t pid;
+    pid_t pid; 
+    const char *s = " "; /* delimiter for strtok token*/
 
     while (should_run) {
 
-        int argsSize = -1;
+        int i = 0; /* keeps amount of arguments in input*/
         printf("osh> ");
         fflush(stdout);
+        char input[100]; /* char array where user input is stored*/
+        char *token; 
 
-        char input[inputSize];
+        //gets user input and gets rid of all characters not used in array
+        fgets(input, sizeof(input), stdin);
+        input[strlen(input) - 1] = '\0';
 
-        scanf("%s", input);
+        //stops segmentation fault from empty input
+        if(strlen(input) > 0) {
 
-        for(int i = 0; i < inputSize; i++) {
-            char *token = strtok(input, " ");
+            //token function that finds all the arguments in the input and puts them into args array
+            token = strtok(input, s);
 
-            if(token != NULL){
+            while(token != NULL) {
+
                 args[i] = token;
+
+                i++;
+                token = strtok(NULL, s);
             }
+
+            //changes last argument to NULL so that execvp function knows when command ends
+            args[i] = NULL; 
+
+            //checks whether the function has a '&' and then waits if there isn't one
+            if(args[(i - 1)][0] != '&') {
+                    wait(NULL);
+                }
             else {
-
-                argsSize = i - 1;
-                break;
+                args[i - 1] = NULL;
             }
+
+            //creates fork process where the child will perform shell commands based on inputted commands from user
+            pid = fork();
+
+            if(pid < 0) {
+                exit(0);
+            }
+            else if (pid == 0){
+
+                execvp(args[0], args);
+                exit(0);
+            }
+
+            //checks if the input was exit and then ends the loop if it was to end the program
+            if(strcmp(args[0], "exit") == 0) {
+                should_run = 0;
+            }
+
         }
-
-        // pid = fork();
-
-        // if(pid < 0) {
-        //     exit(0);
-        // }
-        // else if (pid == 0){
-
-        //     execvp(args[0], args);
-        //     exit(0);
-        // }
-        // else {
-
-        //     if(args[argsSize][0] != '&') {
-        //         wait(NULL);
-        //     }
-        //     else {
-        //         exit(0);
-        //     }
-        // }
-
-        printf("%s\n", input);
-        
+ 
     }
 
     return 0;
